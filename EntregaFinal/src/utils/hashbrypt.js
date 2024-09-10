9b6b7460bcd7f9486586471a14e17db090042d0b
Gabriel Núñez Dev <81633637+Gabonun@users.noreply.github.com>
2024-09-10 11:45:41 -0300

    Delete EntregaFinal/src directory
    Se sube proyecto actualizado

--- EntregaFinal/src/utils/hashbrypt.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from "bcrypt";
-import { create } from "express-handlebars";
-
-const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-const isValidPassword = (password, user) => bcrypt.compareSync(password, user.password);
-
-export {createHash, isValidPassword};
