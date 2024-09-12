8e5ffc4c395ef4760734e0d91576440d8458eef8
Gabriel Núñez Dev <81633637+Gabonun@users.noreply.github.com>
2024-09-12 10:30:19 -0300

    Delete ProyectoFinal directory
    Habían errores generales en la subida por lo que se decidió subir una nueva compilación en una nueva carpeta

--- ProyectoFinal/src/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import configObject from "./config/config.js";
-
-const { MONGO_URL } = configObject
-
-mongoose.connect(MONGO_URL)
-    .then(() => console.log("Base de datos conectada correctamente"))
-    .catch((error) => console.log("Hay algun error", error))
--- ProyectoFinal/src/utils/commander.js
+++
@@ -1,9 +0,0 @@
-import { Command } from "commander";
-const program = new Command(); 
-
-program
-    .option("-p <port>", "Puerto en donde se inicia el servidor", 8080)
-    .option("--mode <mode>", "modo de trabajo", "desarrollo")
-program.parse(); 
-
-export default program; 
