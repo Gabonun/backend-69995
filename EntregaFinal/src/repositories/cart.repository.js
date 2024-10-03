6809376567ffaafc9a78e155beaeb8dcef1c3a11
Gabriel Núñez Dev <81633637+Gabonun@users.noreply.github.com>
2024-10-02 22:43:22 -0300

    Delete EntregaFinal/src directory

--- EntregaFinal/src/repositories/cart.repository.js
+++
@@ -1,9 +0,0 @@
-import CartDao from "../dao/models/cart.dao.js";
-
-class CartRepository {
-    async createcart() {
-        return await CartDao.create();
-    }
-}
-
-export default new CartRepository();
